b373eb497c67fcb900cf0a452d42f7fa139bdd36
Eme087 <159288546+Eme087@users.noreply.github.com>
2025-06-20 18:47:59 +0000

    Añadido pausa al final del proceso en sync_main

--- sync_main.py
+++ sync_main.py
@@ -2,5 +2,4 @@ from sync_stock import main
 
 if __name__ == "__main__":
     main()
-    input("\n🔚 Proceso finalizado. Presiona Enter para salir...")
-
+    input("\n🔚 Proceso finalizado. Presiona ENTER para cerrar la ventana.")
